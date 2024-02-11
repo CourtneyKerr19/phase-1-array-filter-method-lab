3e001be61abe1c0c7a7688440920e573f171b093
Courtney Kerr <courtneykerr219@gmail.com>
2024-02-11 00:50:42 -0600

    complete

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution here
+// findMatching function
+function findMatching(drivers, query) {
+    return drivers.filter(driver => driver.toLowerCase() === query.toLowerCase());
+  }
+  
+  // fuzzyMatch function
+  function fuzzyMatch(drivers, letters) {
+    return drivers.filter(driver => driver.startsWith(letters));
+  }
+  
+  // matchName function
+  function matchName(drivers, name) {
+    return drivers.filter(driver => driver.name === name);
+  }
